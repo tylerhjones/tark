827871a24c232a70221d6a0b308f2f6e85d9e94e
Tyler Jones <tyler.jones@logmein.com>
2022-04-25 22:29:47 -0600

    bump version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as readme_file:
 
 setup(
     name="tark",
-    version="0.0.2",
+    version="0.0.3",
     author="tyler jones",
     description="Markdown parser focused on blogs.",
     long_description=readme,
